fd15be646a72cca9f6993e1f783c24caac0b4d5e
theProgrammingBox <justinboxgea@gmail.com>
2022-06-23 19:33:31 -0700

    h

--- Triangle.cpp
+++ Triangle.cpp
@@ -2,10 +2,10 @@
 
 double Triangle::calcPerimeter() const
 {
-	return 3 * length;
+	return 3.0 * length;
 }
 
 double Triangle::calcArea() const
 {
-	return (length * length) / 2;
+	return (length * length) / 2.0;
 }
